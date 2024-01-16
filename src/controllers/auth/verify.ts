66c70acc96eb003d3b0aaa37b372428f4c0691cc
Computewell <ololade220@gmail.com>
2024-01-16 17:24:44 +0100

    verification

--- src/controllers/auth/verify.ts
+++ src/controllers/auth/verify.ts
@@ -1,2 +1,23 @@
 import { Request, Response } from "express";
-const verify = async (req: Request, res: Response) => {};
+import UserModel from "../../models/UserModel";
+export const verify = async (req: Request, res: Response) => {
+    const { token } = req.params;
+    if (!token) {
+        return res.status(400).json({ err: "Invalid verification token" });
+    }
+
+    try {
+        // Find the user with the given token
+        const user = await UserModel.findOne({ verificationToken: token });
+        if (!user) {
+            return res.status(404).json({ err: "User not found or already verified" });
+        }
+        // Update the user's verification status to true
+        user.verified = true;
+        await user.save();
+    } catch (err) {
+        console.log("Error verifying user: ", err);
+        return res.status(500).json({ err: "Internal server error" });
+    }
+
+};
