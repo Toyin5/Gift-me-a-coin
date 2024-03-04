80b050c50ce6a7e2315ff2b6e46e841807c79cc5
dchefcoder <ololade220@gmail.com>
2024-03-04 10:34:11 +0100

    updateResetPassword

--- src/controllers/auth/resetPassword.ts
+++ src/controllers/auth/resetPassword.ts
@@ -1,34 +1,44 @@
 import { Request, Response } from "express";
 import UserModel from "../../models/UserModel";
 import "dotenv/config";
-import bcrypt from "bcrypt";
 import { DateTime } from "luxon";
 import logger from "../../utils/logger";
+import { sendMail } from "../../services/sendMail";
 
-// Reset Password Implementation
 export const resetPassword = async (req: Request, res: Response) => {
     const {token, newPassword} = req.body;
     try {
-        if(!token || !newPassword) {
-            return res.status(404).json({message: "Invalid token or password"})
+        if (!token || !newPassword) {
+            return res.status(404).json({ message: "Invalid token or password" });
         }
-        const user = await UserModel.findOne({passwordResetToken: token}).select("+passwordResetExpires");
+        const user = await UserModel.findOne({ passwordResetToken: token }).select("+passwordResetExpires");
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return res.status(400).json({ message: "User not found" });
         }
         if (DateTime.now().toJSDate() > user.passwordResetExpires) {
-            return res.status(404).json({ message: "Token expired" });
+            return res.status(404).json({ message: "Token has expired" });
         }
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
         await UserModel.findByIdAndUpdate(user._id, {
-            password: hashedPassword,
+            password: newPassword,
+            passwordConfirm: "",
             passwordResetToken: "",
             passwordResetExpires: "",
             passwordResetRetries: 0,
         });
+        const url = `${process.env.CLIENT_URI}/login`;
+        await sendMail({
+            type: "resetPassword",
+            data: {
+                name: user.firstName,
+                to: user.email,
+                url,
+                priority: "1",
+            },
+        });
         return res.status(200).json({ message: "Password reset successfully" });
-    } catch (error) {
+    }
+    catch (error) {
         logger.error(error);
         return res.status(500).json({ message: "Internal server error" });
     }
-}
+};
